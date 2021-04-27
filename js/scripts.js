2368cf53555ecb1c65dcd6beaafa51f5c1a04348
Lucian <lucian.miller185@gmail.com>
2021-04-27 10:52:53 -0700

    It will fix error with information gathering with job question

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     const hobbyAnswer = parseInt($("input:radio[name=hobby]:checked").val());
     const feelingsAnswer = parseInt($("input:radio[name=feelings]:checked").val());
-    const jobAnswer = parseInt($("input:radio[name=job].checked").val());
+    const jobAnswer = parseInt($("input:radio[name=job]:checked").val());
     const creatureAnswer = parseInt($("input:radio[name=creature]:checked").val());
     const fearAnswer = parseInt($("input:radio[name=fear]:checked").val());
 
