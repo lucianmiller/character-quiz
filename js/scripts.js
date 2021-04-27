b2c8b20d5fa41d2f2028f14246dccffdf922d0d2
Lucian <lucian.miller185@gmail.com>
2021-04-27 10:59:01 -0700

    It will add answerTotal variable

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,14 @@ $(document).ready(function() {
     const jobAnswer = parseInt($("input:radio[name=job]:checked").val());
     const creatureAnswer = parseInt($("input:radio[name=creature]:checked").val());
     const fearAnswer = parseInt($("input:radio[name=fear]:checked").val());
+    const answerTotal = hobbyAnswer + feelingsAnswer + jobAnswer + creatureAnswer + fearAnswer
 
     console.log("hobby: " + hobbyAnswer);
     console.log("feeling: " + feelingsAnswer);
     console.log("job: " + jobAnswer);
     console.log("creature: " + creatureAnswer);
     console.log("fear: " + fearAnswer);
+    console.log("answer total: " + answerTotal)
 
     event.preventDefault();
   });
